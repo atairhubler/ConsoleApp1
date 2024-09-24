18f1e0279ae6726c9a68964937f2d3a9dac9bc7d
Atair Hubler Junior <atairjrbr@gmail.com>
2024-09-24 08:58:37 -0300

    Atividade do estoque resolvida com exito

---
+++ ConsoleApp7/Estoque.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Estoque
+    {
+        Produto Produto1 {  get; set; } = new Produto();
+        List<string> listaEstoque = new List<string>();
+
+        public void AdicionarProdutos(string nome) 
+        { 
+            listaEstoque.Add(nome);
+        }
+        public void ExibirProdutos()
+        {
+            foreach (var produto in listaEstoque)
+            {
+                Console.WriteLine(produto.ToString());
+            }
+        }
+    }
+}
+
+
+/*
+
+Classe Estoque
+    Nome
+
+
+Funcao AdicionarProdutos
+Funcao ExibirProdutos
+
+*/
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,111 @@
+﻿
+
+
+using ConsoleApp7;
+
+Estoque estoque1 = new Estoque();
+
+estoque1.AdicionarProdutos("Martelo");
+estoque1.AdicionarProdutos("Martelo2");
+estoque1.AdicionarProdutos("Martelo3");
+estoque1.AdicionarProdutos("Martelo4");
+
+estoque1.ExibirProdutos();
+
+
+
+
+
+
+
+
+/*
+
+//
+
+
+
+Classe Estoque
+    Nome
+
+
+Funcao AdicionarProdutos
+Funcao ExibirProdutos
+
+
+
+
+Desenvolver uma classe que represente um estoque de produtos, e que tenha as funcionalidades de adicionar novos produtos, e exibir todos os produtos no estoque.
+
+
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+
+
+//
+
+
+
+Classe Aluno
+    Nome
+    Idade
+    Notas
+
+
+Classe Professor
+    Nome
+    Disciplinas
+
+
+Classe Disciplina
+    Nome
+    Lista de alunos matriculados
+
+
+
+
+//
+
+
+
+
+Classe Restaurante
+
+
+Classe Mesa
+
+
+Classe Pedido
+
+
+Classe Cardapio
+
+
+
+
+
+
+
+
+
+
+
+
+Modelar o sistema de uma escola. Crie classes para Aluno, Professor e Disciplina. 
+A classe Aluno deve ter informações como nome, idade e notas. A classe Professor deve ter informações sobre nome e disciplinas lecionadas. A classe Disciplina deve armazenar o nome da disciplina e a lista de alunos matriculados.
+
+
+
+Modelar um sistema para um restaurante com classes como Restaurante, Mesa, Pedido e Cardapio. A classe Restaurante deve ter mesas que podem ser reservadas e um cardápio com itens que podem ser pedidos. Os pedidos podem estar associados a uma mesa.
+
+*/
