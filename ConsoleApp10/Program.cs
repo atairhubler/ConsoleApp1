ef6eff30eaf5a369e6c4a03b356bc81ef58b04e6
Atair Hubler Junior <atairjrbr@gmail.com>
2024-09-25 10:41:29 -0300

    Inicio da atividade catalogo de jogos

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+//Desenvolver uma classe que representa um catálogo de jogos, com uma lista de Jogos e métodos para manipular essa lista, bem como um construtor que faça sua inicialização.
